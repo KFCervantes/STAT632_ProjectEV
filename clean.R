b2ccc96f6477d9a80bc09352acd5442baa49b873
KFCervantes <kalebfcervantes@gmail.com>
2022-04-08 02:14:11 +0000

    Added R script for cleaning the data

---
+++ clean.R
@@ -0,0 +1,43 @@
+library(magrittr)
+library(tidyverse)
+
+# load in data
+data <- "Dataset/Cheapestelectriccars-EVDatabase.csv" %>%
+  read.csv
+
+# convert selected columns to float
+data[c("Subtitle", "Acceleration")] %<>%
+  
+  # define new function and apply it on every value in the column
+  lapply(
+    
+    # x is the string
+    function (x)
+      x %>%
+      
+      # keep substring consisting of digits and decimal points
+      str_extract("[\\.\\d]+") %>%
+      
+      # convert string to float
+      as.numeric
+  )
+
+# convert selected columns to int
+data[c("TopSpeed", "Range", "Efficiency", "FastChargeSpeed", "PriceinGermany", "PriceinUK")] %<>%
+  
+  # define new function and apply it on every value in the column
+  lapply(
+    
+    # x is the string
+    function (x)
+      x %>%
+      
+      # remove comma from string
+      str_remove(",") %>%
+      
+      # keep substring consisting of digits
+      str_extract("\\d+") %>%
+      
+      # convert string to int
+      as.integer
+  )
